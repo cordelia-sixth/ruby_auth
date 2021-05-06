9b8c52bdfef8ece253b947960fc4ddb2ca871639
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-05-06 10:14:38 +0900

    Update basic_auth.rb

--- basic_auth.rb
+++ basic_auth.rb
@@ -1,60 +1,31 @@
 require 'webrick'
 
 # HTTPサーバーを作成
-server = WEBrick::HTTPServer.new({
+server = WEBrick::HTTPServer.new(
   DocumentRoot: "./",
   BindAddress: "127.0.0.1",
   Port: 8000
-})
+)
 
 # 認証情報を記載するファイルを作成
-pswd = WEBrick::HTTPAuth::Htpasswd.new("dot.passwd")
+passwd = WEBrick::HTTPAuth::Htpasswd.new(".passwd")
 
-# 認証情報を設定
-pswd.set_passwd("my_realm", "username", "password")
-
-auth = WEBrick::HTTPAuth::BasicAuth.new(UserDB: pswd, Realm: "my_realm")
+# 認証情報をセットする
+passwd.set_passwd("my_realm", "username", "password")
 
 # Basic認証を設定する
+auth = WEBrick::HTTPAuth::BasicAuth.new(UserDB: passwd, Realm: "my_realm")
+
+# Basic認証をかける
 server.mount_proc("/admin") do |req, res|
 
-  # 認証
+  # 認証情報をチェックする
   auth.authenticate(req, res)
-  res.body = "Welcome my page!"
+  res.body = "Welcome!"
 end
 
-trap("INT"){server.stop}
+# ctrl + C 押されたらサーバーを終了する
+trap("SIGINT") { server.stop }
+
+# サーバー起動
 server.start
-
-
-
-
-# # realmを定義
-# realm = "WEBrick realm"
-
-# # サーバー設定
-# server = WEBrick::HTTPServer.new({
-
-#   # ドキュメントルート
-#   DocumentRoot: '.',
-
-#   # IPアドレス
-#   BindAddress: '127.0.0.1',
-
-#   # ポート番号
-#   Port: 8000
-# })
-
-# # 認証情報を記載するファイルを作成
-# htpd = WEBrick::HTTPAuth::Htpasswd.new("dot.htpasswd")
-
-# htpd.set_passwd(nil, "username", "password")
-
-# authenticator = WEBrick::HTTPAuth::BasicAuth.new(UserDB: htpd, Realm: realm)
-
-# server.mount_proc("/basic_auth") { | req, res |
-#   authenticator.authenticate(req, res)
-#   res.body = 'hoge'
-# }
-
-# server.start
